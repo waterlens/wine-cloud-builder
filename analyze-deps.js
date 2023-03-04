7e75e19670af2ed151be6683efbc56d9146f5d3b
3Shain <san3shain@outlook.com>
2023-03-04 03:47:11 +0000

    check -Os

--- analyze-deps.js
+++ analyze-deps.js
@@ -9,7 +9,7 @@ const realpath = promisify(require("fs").realpath);
 const readFile = promisify(require("fs").readFile);
 const symlink = promisify(require("fs").symlink);
 const fsConstants = require("fs").constants;
-const koffi = require('koffi');
+const koffi = require("koffi");
 
 // return a list of deps
 async function getDeps(name) {
@@ -30,27 +30,25 @@ async function changeDep(from, to, file) {
 }
 
 async function changeId(to, file) {
-    const { stdout: output } = await exec(
-      `install_name_tool -id ${to} ${file}`
-    );
-  }
+  const { stdout: output } = await exec(`install_name_tool -id ${to} ${file}`);
+}
 
 async function doCopy(from) {
-    const file = path.basename(from);
-    const s = await lstat(from);
-    if((s).isSymbolicLink()) {
-        const real = await realpath(from);
-        try {
-            await access(path.basename(real),fsConstants.F_OK)
-        } catch {
-            await copy(real, path.basename(real));
-        }
-        if(path.basename(real)!=file) {
-            await symlink(path.basename(real), file)
-        }
-    } else {
-        await copy(from, file);
+  const file = path.basename(from);
+  const s = await lstat(from);
+  if (s.isSymbolicLink()) {
+    const real = await realpath(from);
+    try {
+      await access(path.basename(real), fsConstants.F_OK);
+    } catch {
+      await copy(real, path.basename(real));
     }
+    if (path.basename(real) != file) {
+      await symlink(path.basename(real), file);
+    }
+  } else {
+    await copy(from, file);
+  }
 }
 
 async function analyzeLib(name) {
@@ -62,7 +60,7 @@ async function analyzeLib(name) {
     console.log(`processing ${self}`);
     await changeId(`@rpath/${path.basename(self)}`, name);
     for (const dep of deps.slice(1)) {
-      if (!dep.startsWith("/usr/lib/")&&!dep.startsWith('/System/Library/')) {
+      if (!dep.startsWith("/usr/lib/") && !dep.startsWith("/System/Library/")) {
         try {
           await access(path.basename(dep), fsConstants.F_OK);
         } catch {
@@ -76,45 +74,43 @@ async function analyzeLib(name) {
 }
 
 async function find(file) {
-   try {
-        koffi.load('/usr/lib/'+file);
-        return '';
-   } catch {
-    console.log(`Seems ${file} is not a system library, finding in brew...`)
-   }
-    const { stdout: output } = await exec(
-        'find /usr/local/Cellar -name '+file
-      );
-    const files = output.split('\n').filter(x=>x!='');
-    if(files.length==0) {
-        throw new Error(`Can not find ${file}!`);
-    }
-    if(files.length>1) {
-        console.log(`Find multiple ${file}`);
-        console.log(files);
-    }
-    return files[0];
+  try {
+    koffi.load("/usr/lib/" + file);
+    return "";
+  } catch {
+    console.log(`Seems ${file} is not a system library, finding in brew...`);
+  }
+  const { stdout: output } = await exec("find /usr/local/Cellar -name " + file);
+  const files = output.split("\n").filter((x) => x != "");
+  if (files.length == 0) {
+    throw new Error(`Can not find ${file}!`);
+  }
+  if (files.length > 1) {
+    console.log(`Find multiple ${file}`);
+    console.log(files);
+  }
+  return files[0];
 }
 
 async function main() {
-    const header = await readFile(process.argv[2],{encoding:'utf-8'});
-    const tofind = header.split('\n')
-    .map(x=>x.replace(/\s+/g, ' ').trim().split(' '))
-    .filter(array=>array[0]=='#define'&&array[1]?.startsWith('SONAME_'))
-    .map(array=>array[2].slice(1,-1));
-    console.log(tofind);
-    for(const file of tofind) {
-        const abspath = await find(file);
-        // FIXME: wtf?
-        if(abspath=='') continue;
-        try {
-            await access(path.basename(abspath),fsConstants.F_OK);
-        } catch {
-            await doCopy(abspath);
-        }
-        await analyzeLib(path.basename(abspath));
+  const header = await readFile(process.argv[2], { encoding: "utf-8" });
+  const tofind = header
+    .split("\n")
+    .map((x) => x.replace(/\s+/g, " ").trim().split(" "))
+    .filter((array) => array[0] == "#define" && array[1]?.startsWith("SONAME_"))
+    .map((array) => array[2].slice(1, -1));
+  console.log(tofind);
+  for (const file of tofind) {
+    const abspath = await find(file);
+    // FIXME: wtf?
+    if (abspath == "") continue;
+    try {
+      await access(path.basename(abspath), fsConstants.F_OK);
+    } catch {
+      await doCopy(abspath);
     }
+    await analyzeLib(path.basename(abspath));
+  }
 }
 
-main().then((x) => {
-});
+main().then((x) => {});
